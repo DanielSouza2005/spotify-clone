1ad3a7c135e33f4d9f3ac15cb0ef7fda2befcd8b
DanielSouza2005 <danisimoes741@gmail.com>
2025-03-04 11:48:58 -0300

    💄Added the play button; Minor Visual Improvements

--- src/components/ConteudoPrincipal/HomePlaylists/MostListenedPlaylistsItem/index.tsx
+++ src/components/ConteudoPrincipal/HomePlaylists/MostListenedPlaylistsItem/index.tsx
@@ -1,5 +1,7 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { iPlaylist } from "../../../../shared/interfaces/iPlaylists";
 import "./MostListenedPlaylistsItem.css";
+import { faPlay } from "@fortawesome/free-solid-svg-icons";
 
 interface MostListenedPlaylistsProps {
     playlistsData: iPlaylist
@@ -10,6 +12,12 @@ const MostListenedPlaylistsItem = ({ playlistsData }: MostListenedPlaylistsProps
         <div className="cards">
             <div className="cards__container">
                 <img src={playlistsData.foto} alt={playlistsData.alt} loading="lazy" />
+                <div className="play">
+                    <FontAwesomeIcon
+                        icon={faPlay}
+                        className="fa__play__icon"
+                    />
+                </div>
             </div>
             <div className="cards__info__container">
                 <span>{playlistsData.nome}</span>
--- src/components/ConteudoPrincipal/HomePlaylists/index.tsx
+++ src/components/ConteudoPrincipal/HomePlaylists/index.tsx
@@ -23,7 +23,6 @@ const HomePlaylists = () => {
                 </div>
 
                 <MostListenedPlaylists />
-                {/* <MostListenedPlaylists /> */}
             </div>
         </div>
     );
